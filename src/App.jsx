9e787fc12352ca2c1a60ebd0bec7a9264f6afd7e
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-09-19 12:38:14 -0600

    Project scafold

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import  Signup  from './Pages/Signup'
+
+function App() {
+
+  return (
+    <>
+      <p>React Auth</p>
+      <Home />
+      <Dashboard />
+      <Login />
+      <Secret />
+      <Signup />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export coh1 Dashboard =h1 => {
+  return (
+    <h1>Dashboard</h1>
+  )
+}
