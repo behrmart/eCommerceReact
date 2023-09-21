af908e830ac26fddfc9595c7284888e303155e7a
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-09-20 21:44:11 -0600

    Singup working ok with API!

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+import '@/Styles/Form.css'
+import logo from '@/assets/react.svg'
+import { useForm } from "react-hook-form";
+import { registerUserService } from '../Services/UserService';
+import { useNavigate } from 'react-router-dom';
 
 export const Signup = () => {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const navigate = useNavigate()
+  const onSubmit = async (data) => {
+    try {
+      const response = await registerUserService(data)
+      console.log(response)
+      if (response.status == 201) {
+        navigate('/login')
+        console.log('Usuario creado exitosamente')
+      }
+    } catch (error) {
+      console.log('Error en signup ', error)
+    }
+  }
+
   return (
-    <h1>Signup</h1>
+    <main className='form-signin w-100 m-auto'>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <img className='mb-4' src={logo} alt='' width='72' height='57' />
+      <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
+
+      <div className='form-floating'>
+        <input
+          type='text'
+          className='form-control'
+          id='first_name'
+          name='first_name'
+          
+          
+          placeholder='John'
+          {...register('first_name')}
+        />
+        <label htmlFor='first_name'>First Name</label>
+      </div>
+
+      <div className='form-floating'>
+        <input
+          type='text'
+          className='form-control'
+          id='last_name'
+          name='last_name'
+          placeholder='Doe'
+          {...register('last_name')}
+        />
+        <label htmlFor='last_name'>Last Name</label>
+      </div>
+
+      <div className='form-floating'>
+        <select
+          className='form-select'
+          id='gender'
+          name='gender'
+          {...register('gender')}
+        >
+          <option value=''>Choose...</option>
+          <option value='M'>Male</option>
+          <option value='F'>Female</option>
+        </select>
+        <label htmlFor='gender'>Gender</label>
+      </div>
+
+      <div className='form-floating'>
+        <input
+          type='email'
+          className='form-control'
+          id='email'
+          name='email'
+          placeholder='name@example.com'
+          {...register('email')}
+        />
+        <label htmlFor='email'>Email address</label>
+      </div>
+
+      <div className='form-floating'>
+        <input
+          type='password'
+          className='form-control'
+          id='password'
+          name='password'
+          placeholder='Password'
+          {...register('password')}
+        />
+        <label htmlFor='password'>Password</label>
+      </div>
+
+      <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
+      <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+    </form>
+  </main>
   )
 }
