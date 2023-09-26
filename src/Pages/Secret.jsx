0f242b0ddaaf2e12e632d5446c5df32a902815e8
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-09-26 17:24:59 -0600

    Otra forma de user payload

--- src/Pages/Secret.jsx
+++ src/Pages/Secret.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useAuthContext } from "../Hooks/useAuthContext"
 
 export const Secret = () => {
+
+  const { userPayload } = useAuthContext()
+
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      {
+        userPayload?.role === 'ADMIN'  //"Optional chaining" No pinta nada si no existe
+          ? <h2>Hola ADMIN</h2>
+          : <h2>Hola CUSTOMER</h2>
+      }
+      {userPayload?.role === 'ADMIN' && <h2>Hola Admin</h2>}
+      {userPayload?.role === 'CUSTOMER' && <h2>Saludos Customer</h2>}
+    </>
   )
 }
