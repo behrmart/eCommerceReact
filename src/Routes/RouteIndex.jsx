42d121ef84c4ac3eafd73947bb3a48ca9b2dd9ec
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-09-26 17:13:14 -0600

    Protected Routes

--- src/Routes/RouteIndex.jsx
+++ src/Routes/RouteIndex.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Home, Dashboard, Login, Secret, Signup } from '@/Pages'
+import { useAuthContext } from '../Hooks/useAuthContext'
 
 export const RouteIndex = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/secret' element={<Secret />} />
-        <Route path='/signup' element={<Signup />} />
+      <Route path='/' element={<Home />} />
+      <Route
+        path='/dashboard'
+        element={isAuth ? <Dashboard /> : <Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route
+        path='/secret'
+        element={isAuth ? <Secret /> : <Login />} />
+      <Route path='/signup' element={<Signup />} />
     </Routes>
   )
 }
