b978794d77d2bd1c46455066b015e4065df2c68b
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-09-26 17:56:07 -0600

    Dashboard con info del usuario

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { getMeUserService } from '../Services/UserService'
+import { useState, useEffect } from 'react'
 
 export const Dashboard = () => {
+
+  const [userData, setUserData] = useState({})
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getMeUserService(token)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Errror en Dashboard ', error)
+      }
+    }
+    fetchUserData()
+  }, [token])
+
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      {userData?.first_name && <p>{userData.first_name}</p>}
+      {userData?.last_name && <p>{userData.last_name}</p>}
+      {userData?.gender && <p>{userData.gender}</p>}
+      {userData?.email && <p>{userData.email}</p>}
+      {userData?.role && <p>{userData.role}</p>}
+    </>
   )
 }
