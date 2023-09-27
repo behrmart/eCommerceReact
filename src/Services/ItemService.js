f81592dc43c5fbadc27f0051e1b7e6aae525588b
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-09-26 18:28:16 -0600

    finish home items

--- src/Services/ItemService.js
+++ src/Services/ItemService.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
 const getAllItemsService =  () => axios.get(`${BASE_URL}/items`)
 const getOneItemService = (id) => axios.get(`${BASE_URL}/items/${id}`)
-const createItemService = (data, jwtToken) = axios.post(`${BASE_URL}/items/`, data, { headers: {Authorization: `Bearer ${jwtToken}`}})
+const createItemService = (data, jwtToken) => axios.post(`${BASE_URL}/items/`, data, { headers: {Authorization: `Bearer ${jwtToken}`}})
 
 export {
     getAllItemsService,
